6a44a60574e30a04a18dbf44cb4d70efed3192b9
Tomas Verhelst <tve@teletask.be>
2021-03-11 10:17:13 +0100

    Migrated to null safety

--- lib/msgpack_dart.dart
+++ lib/msgpack_dart.dart
@@ -6,21 +6,27 @@ import 'dart:typed_data';
 
 part 'src/common.dart';
 part 'src/data_writer.dart';
-part 'src/serializer.dart';
 part 'src/deserializer.dart';
+part 'src/serializer.dart';
 
 Uint8List serialize(
   dynamic value, {
-  ExtEncoder extEncoder,
+  ExtEncoder? extEncoder,
 }) {
   final s = Serializer(extEncoder: extEncoder);
   s.encode(value);
   return s.takeBytes();
 }
 
-dynamic deserialize(Uint8List list,
-    {ExtDecoder extDecoder, bool copyBinaryData = false}) {
-  final d = Deserializer(list,
-      extDecoder: extDecoder, copyBinaryData: copyBinaryData);
+dynamic deserialize(
+  Uint8List list, {
+  ExtDecoder? extDecoder,
+  bool copyBinaryData = false,
+}) {
+  final d = Deserializer(
+    list,
+    extDecoder: extDecoder,
+    copyBinaryData: copyBinaryData,
+  );
   return d.decode();
 }
